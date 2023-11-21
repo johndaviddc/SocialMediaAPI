78bafdd6e648b6739f458c8564893428536d8362
John David Dela Cruz <johndaviddc1@gmail.com>
2023-11-21 15:41:54 +0800

    social media

---
+++ src/test/java/dave/dev/socialmedia/SocialmediaApplicationTests.java
@@ -0,0 +1,13 @@
+package dave.dev.socialmedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialmediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
