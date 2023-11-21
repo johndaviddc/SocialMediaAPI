ed64a2db23b189840b14c495967e1c589b1007e4
John David Dela Cruz <johndaviddc1@gmail.com>
2023-11-21 15:48:01 +0800

    added post model

--- src/main/java/dave/dev/socialmedia/model/Post.java
+++ src/main/java/dave/dev/socialmedia/model/Post.java
@@ -14,4 +14,53 @@ public class Post {
 
     @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Comment> comments = new ArrayList<>();
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Like> likes = new ArrayList<>();
+
+    public Post(Long id, String content, List<Comment> comments, List<Like> likes) {
+        this.id = id;
+        this.content = content;
+        this.comments = comments;
+        this.likes = likes;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
+
+    public List<Like> getLikes() {
+        return likes;
+    }
+
+    public void setLikes(List<Like> likes) {
+        this.likes = likes;
+    }
+
+    public Post() {
+    }
+
+    public Post(String content) {
+        this.content = content;
+    }
 }
