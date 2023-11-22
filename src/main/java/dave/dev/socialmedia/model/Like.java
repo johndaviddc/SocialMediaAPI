6146689085ed4b3517707a79c4317b94541772e2
John David Dela Cruz <johndaviddc1@gmail.com>
2023-11-22 16:15:42 +0800

    added like model

---
+++ src/main/java/dave/dev/socialmedia/model/Like.java
@@ -0,0 +1,38 @@
+package dave.dev.socialmedia.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Like {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    public Like(Long id, Post post) {
+        this.id = id;
+        this.post = post;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+
+    public Like() {
+    }
+}
