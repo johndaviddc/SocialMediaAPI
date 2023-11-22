a4000c11fcf834ae4f1c7644ae1b6a887f537510
John David Dela Cruz <johndaviddc1@gmail.com>
2023-11-22 16:12:23 +0800

    added comment model

---
+++ src/main/java/dave/dev/socialmedia/model/Comment.java
@@ -0,0 +1,54 @@
+package dave.dev.socialmedia.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    public Comment(Long id, String content, Post post) {
+        this.id = id;
+        this.content = content;
+        this.post = post;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+
+    public Comment() {
+    }
+
+    public Comment(String content) {
+        this.content = content;
+    }
+    
+}
