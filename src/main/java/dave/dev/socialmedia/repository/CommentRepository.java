30dd70648b129989e8299b5f29774f984a0325d6
John David Dela Cruz <johndaviddc1@gmail.com>
2023-11-23 15:30:35 +0800

    added comment repository

---
+++ src/main/java/dave/dev/socialmedia/repository/CommentRepository.java
@@ -0,0 +1,9 @@
+package dave.dev.socialmedia.repository;
+
+import dave.dev.socialmedia.model.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}
