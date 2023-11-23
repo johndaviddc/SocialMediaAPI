6b8a5684f8997b247eb5db999f7f2c6946ac6ad7
John David Dela Cruz <johndaviddc1@gmail.com>
2023-11-23 15:31:34 +0800

    added like repository

---
+++ src/main/java/dave/dev/socialmedia/repository/LikeRepository.java
@@ -0,0 +1,9 @@
+package dave.dev.socialmedia.repository;
+
+import dave.dev.socialmedia.model.Like;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LikeRepository extends JpaRepository<Like, Long> {
+}
