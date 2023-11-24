54479961fc2c2dba83da2e768a23a0080b670bae
John David Dela Cruz <johndaviddc1@gmail.com>
2023-11-24 13:29:17 +0800

    added addCommentToPost

--- src/main/java/dave/dev/socialmedia/service/CommentService.java
+++ src/main/java/dave/dev/socialmedia/service/CommentService.java
@@ -1,7 +1,9 @@
 package dave.dev.socialmedia.service;
 
 import dave.dev.socialmedia.model.Comment;
+import dave.dev.socialmedia.model.Post;
 import dave.dev.socialmedia.repository.CommentRepository;
+import dave.dev.socialmedia.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,8 +13,20 @@ import java.util.Optional;
 public class CommentService {
     @Autowired
     private CommentRepository commentRepository;
+    private PostRepository postRepository;
 
     public Optional<Comment> getCommentById(Long commentId) {
         return commentRepository.findById(commentId);
+    }
+
+    public Comment addCommentToPost(Long postId, Comment comment) {
+        Optional<Post> postOptional = postRepository.findById(postId);
+        postOptional.ifPresent(post -> {
+            comment.setPost(post);
+            post.getComments().add(comment);
+            postRepository.save(post);
+        });
+
+        return comment;
     }
 }
