f984dfab4373a8ad5d9065931dc539d05b6afa7f
John David Dela Cruz <johndaviddc1@gmail.com>
2023-11-25 14:13:01 +0800

    added liketopost

--- src/main/java/dave/dev/socialmedia/service/LikeService.java
+++ src/main/java/dave/dev/socialmedia/service/LikeService.java
@@ -1,7 +1,9 @@
 package dave.dev.socialmedia.service;
 
 import dave.dev.socialmedia.model.Like;
+import dave.dev.socialmedia.model.Post;
 import dave.dev.socialmedia.repository.LikeRepository;
+import dave.dev.socialmedia.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,8 +13,19 @@ import java.util.Optional;
 public class LikeService {
     @Autowired
     private LikeRepository likeRepository;
+    private PostRepository postRepository;
 
     public Optional<Like> getLikeById(Long likeId) {
         return likeRepository.findById(likeId);
+    }
+
+    public Like addLikeToPost(Long postId, Like like) {
+        Optional<Post> postOptional = postRepository.findById(postId);
+        postOptional.ifPresent(post -> {
+            like.setPost(post);
+            post.getLikes().add(like);
+            postRepository.save(post);
+        });
+        return like;
     }
 }
