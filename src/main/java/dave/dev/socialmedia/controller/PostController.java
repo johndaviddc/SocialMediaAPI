2c79a22719237dbe20c2ac8616731b12b9abc165
John David Dela Cruz <johndaviddc1@gmail.com>
2023-11-27 10:07:00 +0800

    added get mapping

--- src/main/java/dave/dev/socialmedia/controller/PostController.java
+++ src/main/java/dave/dev/socialmedia/controller/PostController.java
@@ -3,7 +3,10 @@ package dave.dev.socialmedia.controller;
 import dave.dev.socialmedia.model.Post;
 import dave.dev.socialmedia.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +21,12 @@ public class PostController {
     @GetMapping
     public List<Post> getAllPosts() {
         return postService.getAllPosts();
+    }
+
+    @GetMapping("{postId}")
+    public ResponseEntity<Post> getPosById(@PathVariable Long postId) {
+        return postService.getPostById(postId)
+                .map(post -> new ResponseEntity<>(post, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 }
