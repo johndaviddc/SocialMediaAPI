76681e91c1c5b3befcaa53810bfa4aa55b008680
John David Dela Cruz <johndaviddc1@gmail.com>
2023-11-27 10:04:43 +0800

    added controller package

---
+++ src/main/java/dave/dev/socialmedia/controller/PostController.java
@@ -0,0 +1,22 @@
+package dave.dev.socialmedia.controller;
+
+import dave.dev.socialmedia.model.Post;
+import dave.dev.socialmedia.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/posts")
+public class PostController {
+    @Autowired
+    private PostService postService;
+
+    @GetMapping
+    public List<Post> getAllPosts() {
+        return postService.getAllPosts();
+    }
+}
